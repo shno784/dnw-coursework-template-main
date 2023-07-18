76d3975cd0bb87c32675ae9517e9e363a01a6ca2
Joshua Ollivierre <joshino784@gmail.com>
2023-07-17 22:46:07 -0400

    dfwe

--- routes/reader.js
+++ routes/reader.js
@@ -15,7 +15,7 @@ router.get("/", (req, res, next) => {
 });
 
 //Render Article page
-router.get("/article/:id", (req, res) => {
+router.get("/article/:id", (req, res, next) => {
     const articleId = req.params.id;
     global.db.all("SELECT * FROM article WHERE id = ?", [articleId], function (err, articles) {
         if (err) {
@@ -28,6 +28,29 @@ router.get("/article/:id", (req, res) => {
 });
 
 
+router.post("/like/:id", (req, res, next) => {
+    const articleId = req.params.id;
+    
+    global.db.all("SELECT likes FROM article WHERE id = ?", [articleId], function (err, like) {
+        if (err) {
+            next(err);
+            return;
+        }
 
+        const likes = like.likes || 0;
+
+        if (likes > 0) {
+            console.log("Already liked")
+        } else {
+            global.db.all("UPDATE article SET likes = likes + 1 WHERE user_id = ?", [articleId], function (err) {
+                if (err) {
+                    next(err);
+                    return;
+                }
+            })
+            res.redirect(`/reader/article/${articleId}`)
+        }
+    })
+})
 
 module.exports = router;
